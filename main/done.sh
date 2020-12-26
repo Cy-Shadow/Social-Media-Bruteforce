2fa31823f2161431a3394b0a2932d1db51255991
MrHacker-X <70113933+MrHacker-X@users.noreply.github.com>
2020-12-26 15:39:57 +0530

    Update done.sh

--- main/done.sh
+++ main/done.sh
@@ -6,12 +6,12 @@ echo -e "
 	 Github    : MrHacker-X
          Youtube   : MrHacker X
 
-	 ██████╗  ██████╗ ███╗   ██╗███████╗
-	 ██╔══██╗██╔═══██╗████╗  ██║██╔════╝
-	 ██║  ██║██║   ██║██╔██╗ ██║█████╗
-	 ██║  ██║██║   ██║██║╚██╗██║██╔══╝
-        ██████╔╝╚██████╔╝██║ ╚████║███████╗
-        ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+         ██████╗  ██████╗ ███╗   ██╗███████╗
+         ██╔══██╗██╔═══██╗████╗  ██║██╔════╝
+         ██║  ██║██║   ██║██╔██╗ ██║█████╗
+         ██║  ██║██║   ██║██║╚██╗██║██╔══╝
+         ██████╔╝╚██████╔╝██║ ╚████║███████╗
+         ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚══════╝
 
 Hello guys Now Setup is completed, so you can run this tool by this 'python hackos.py' or 'python3 hackos.py' \nRun tor in another session when you're work on Instagram hack, open another session and type 'tor' and enter for run tor
 
